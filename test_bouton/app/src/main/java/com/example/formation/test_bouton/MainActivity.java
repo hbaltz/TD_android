be12930e003c5fdeab8a4cf63bde7ea832b9a766
hbaltz <hugo.baltz@ensg.eu>
2016-02-09 14:20:48 +0100

    Texte qui s'agrandi

--- test_bouton/app/src/main/java/com/example/formation/test_bouton/MainActivity.java
+++ test_bouton/app/src/main/java/com/example/formation/test_bouton/MainActivity.java
@@ -1,67 +1,58 @@
+/**
+ * Application qui agrandi un texte au toucher
+ * Basée sur un exemple d'openclassroom
+ */
+
 package com.example.formation.test_bouton;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         // Recuperation des ressources :
         String hello = getResources().getString(R.string.hello);
 
-        // Recuperation des zones de texte et des différents widgets :
+        // Recuperation du bouton :
         Button bouton = (Button)findViewById(R.id.bouton);
 
         // Remplissage des zones de texte :
         bouton.setText(hello);
+
+        // On indique que cette classe sera son listener pour l'évènement Touch
+        bouton.setOnTouchListener((View.OnTouchListener) this);
     }
 
+    // Fonction lancée au toucher
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public boolean onTouch(View view, MotionEvent event) {
+        // Comme l'évènement nous donne la vue concernée par le toucher, on le récupère et on le caste en Button
+        Button bouton = (Button)view;
+
+        // On récupère la largeur du bouton
+        int largeur = bouton.getWidth();
+        // On récupère la hauteur du bouton
+        int hauteur = bouton.getHeight();
+
+        // On récupère la coordonnée sur l'abscisse (X) de l'évènement
+        float x = event.getX();
+        // On récupère la coordonnée sur l'ordonnée (Y) de l'évènement
+        float y = event.getY();
+
+        // On change la taille du texte :
+        bouton.setTextSize(Math.abs(x - largeur / 2) + Math.abs(y - hauteur / 2));
+        // Le toucher est fini, on veut continuer à détecter les touchers d'après :
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
