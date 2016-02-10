b62036212ff67d0e72565f09da7bebc5b47878e6
hbaltz <hugo.baltz@ensg.eu>
2016-02-10 15:04:48 +0100

    Prendre phot basique stockage carteSD

--- prendrePhoto/app/src/main/java/com/example/formation/prendrephoto/MainActivity.java
+++ prendrePhoto/app/src/main/java/com/example/formation/prendrephoto/MainActivity.java
@@ -1,20 +1,77 @@
 package com.example.formation.prendrephoto;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+//TODO gérer memoire interne
 
 public class MainActivity extends AppCompatActivity {
+
+    private int TAKE_PHOTO_CODE = 0;
+    public static int count = 0;
+    private Button capture;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Here, we are making a folder named picFolder to store
+        // pics taken by the camera using this application.
+        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/";
+        File newdir = new File(dir);
+        newdir.mkdirs();
+
+        Button capture = (Button) findViewById(R.id.btnCapture);
+        capture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // Here, the counter will be incremented each time, and the
+                // picture taken by camera will be stored as 1.jpg,2.jpg
+                // and likewise.
+                count++;
+                String file = dir+count+".jpg";
+                File newfile = new File(file);
+                try {
+                    newfile.createNewFile();
+                }
+                catch (IOException e)
+                {
+                }
+
+                Uri outputFileUri = Uri.fromFile(newfile);
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+                startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
+            Log.d("CameraDemo", "Pic saved");
+        }
     }
 
 }
