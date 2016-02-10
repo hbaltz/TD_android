3198fadfd40c4559a15840577d8683e79287075d
hbaltz <hugo.baltz@ensg.eu>
2016-02-10 14:04:20 +0100

    Envoie de mail (juste msg)

--- envoieMail/app/src/main/java/com/example/formation/envoiemail/MainActivity.java
+++ envoieMail/app/src/main/java/com/example/formation/envoiemail/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.formation.envoiemail;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+//TODO dans string renseigner les destinatires
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     private String bt = null;
     private String mail = null;
+    private String msg = null;
+    private String to = null;
+    private String cc = null;
+    private String sjt = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +42,50 @@ public class MainActivity extends AppCompatActivity {
         editText.setHint(mail);
         button.setText(bt);
 
+        // Attribution des listeners :
+        button.setOnClickListener(buttonListener);
+
     }
+
+    /**
+     * Gestion des évenements :
+     */
+
+    // Listener du bouton d'envoie
+    private View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.i("Send email", "");
+
+            // Recuperation des ressources :
+            to = getResources().getString(R.string.to);
+            cc = getResources().getString(R.string.cc);
+            sjt = getResources().getString(R.string.sjt);
+
+            // Récupération du message
+            msg = editText.getText().toString();
+
+            String[] TO = {to};
+            String[] CC = {cc};
+            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+            emailIntent.setData(Uri.parse("mailto:"));
+            emailIntent.setType("text/plain");
+
+
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+            emailIntent.putExtra(Intent.EXTRA_CC, CC);
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, sjt);
+            emailIntent.putExtra(Intent.EXTRA_TEXT, msg);
+
+            try {
+                startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                finish();
+                Log.i("Finished sending email...", "");
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(MainActivity.this,
+                        "There is no email client installed.", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    };
 }
