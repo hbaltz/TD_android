0f4bc6386099e48130c144a3d1be11bc6d1ec288
hbaltz <hugo.baltz@ensg.eu>
2016-02-10 09:47:35 +0100

    def des critères

--- localisation/app/src/main/java/com/example/formation/localisation/MainActivity.java
+++ localisation/app/src/main/java/com/example/formation/localisation/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.formation.localisation;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Accès au service de localisation :
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+
+        // Récupération des fournisseurs de position utilisables :
+        ArrayList<LocationProvider> providers = new ArrayList<LocationProvider>();
+        ArrayList<String> names = (ArrayList<String>) locationManager.getProviders(true);
+
+        for(String name : names) {
+            providers.add(locationManager.getProvider(name));
+        }
+        
+        /**
+         * Définition des critères pour la sélection du fournisseur
+         */
+
+        Criteria critere = new Criteria();
+
+        // Pour indiquer la précision voulue
+        // On peut mettre ACCURACY_FINE pour une haute précision ou ACCURACY_COARSE pour une moins bonne précision
+        critere.setAccuracy(Criteria.ACCURACY_FINE);
+
+        // Est-ce que le fournisseur doit être capable de donner une altitude ?
+        critere.setAltitudeRequired(true);
+
+        // Est-ce que le fournisseur doit être capable de donner une direction ?
+        critere.setBearingRequired(true);
+
+        // Est-ce que le fournisseur peut être payant ?
+        critere.setCostAllowed(false);
+
+        // Pour indiquer la consommation d'énergie demandée
+        // Criteria.POWER_HIGH pour une haute consommation, Criteria.POWER_MEDIUM pour une consommation moyenne et Criteria.POWER_LOW pour une basse consommation
+        critere.setPowerRequirement(Criteria.POWER_HIGH);
+
+        // Est-ce que le fournisseur doit être capable de donner une vitesse ?
+        critere.setSpeedRequired(true);
+
+
+
     }
 }
