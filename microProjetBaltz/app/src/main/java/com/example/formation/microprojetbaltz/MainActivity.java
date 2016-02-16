81652d7088cdd2c45b5da925323844a1d811f194
hbaltz <hugo.baltz@ensg.eu>
2016-02-16 15:07:04 +0100

    Photo

--- microProjetBaltz/app/src/main/java/com/example/formation/microprojetbaltz/MainActivity.java
+++ microProjetBaltz/app/src/main/java/com/example/formation/microprojetbaltz/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.formation.microprojetbaltz;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,10 +23,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -39,6 +51,12 @@ public class MainActivity extends AppCompatActivity{
     private ArrayList<LocationProvider> providers;
 
     private String msg, desc, env, photo;
+
+    private static int count = 0;
+    private int TAKE_PHOTO_CODE = 0;
+    private int CAMERA_PIC_REQUEST = 2;
+
+    private String mCurrentPhotoPath = "/data/data/com.example.formation.microprojetbaltz/picFolder/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,6 +107,7 @@ public class MainActivity extends AppCompatActivity{
 
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
 
+            // Si la position change
             @Override
             public void onLocationChanged(Location location) {
                 latitude = location.getLatitude();
@@ -104,6 +123,7 @@ public class MainActivity extends AppCompatActivity{
                 Log.d("GPS", msg);
             }
 
+            // Si le gps change d'état
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
                 String newStatus = "";
@@ -124,6 +144,7 @@ public class MainActivity extends AppCompatActivity{
                 Log.d("GPS", msg);
             }
 
+            // Si le gps est activé
             @Override
             public void onProviderEnabled(String provider) {
                 msg = String.format(
@@ -133,6 +154,7 @@ public class MainActivity extends AppCompatActivity{
                 Log.d("GPS", msg);
             }
 
+            // Si le gps est désactivé
             @Override
             public void onProviderDisabled(String provider) {
                 msg = String.format(
@@ -144,16 +166,84 @@ public class MainActivity extends AppCompatActivity{
         });
     }
 
-    // Uniquement pour le bouton imc :
+    // Uniquement pour le bouton env :
     private View.OnClickListener buttonEnvListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             if(!checkPhoto.isChecked()) {
                 Toast.makeText(MainActivity.this, "Merci", Toast.LENGTH_SHORT).show();
             }else{
-
-
+                takephoto();
             }
         }
     };
+
+    public void takephoto() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
+                //result.setText("startActivityForResult");
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_PIC_REQUEST && resultCode == RESULT_OK) {
+            //result.setText("1");
+
+            Bitmap thumbnail;
+            if (data != null) {
+                if (data.hasExtra("data")) {
+                    thumbnail = data.getParcelableExtra("data");
+                    ImageView image = (ImageView) findViewById(R.id.photoResultView);
+                    image.setImageBitmap(thumbnail);
+                    //result.setText("2");
+                }
+            } else {
+                // On sait ici que le fichier pointé par mFichier est accessible, on peut donc faire ce qu'on veut avec, par exemple en faire un Bitmap
+                Bitmap image = BitmapFactory.decodeFile(mCurrentPhotoPath);
+                thumbnail = ThumbnailUtils.extractThumbnail(image, 100, 100);
+
+                ImageView imageview = (ImageView) findViewById(R.id.photoResultView);
+                imageview.setImageBitmap(thumbnail);
+
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        //Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM);
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+
+        //result.setText(mCurrentPhotoPath);
+        return image;
+    }
 }
--- prendrePhoto/app/src/main/java/com/example/formation/prendrephoto/MainActivity.java
+++ prendrePhoto/app/src/main/java/com/example/formation/prendrephoto/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.formation.prendrephoto;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -9,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,15 +20,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
-//TODO gérer memoire interne
 
 public class MainActivity extends AppCompatActivity {
 
     private int TAKE_PHOTO_CODE = 0;
+    private int CAMERA_PIC_REQUEST = 2;
     public static int count = 0;
     private Button capture;
+    private String mCurrentPhotoPath = "/data/data/com.example.formation.microprojetbaltz/picFolder/";
 
 
     @Override
@@ -34,13 +41,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Here, we are making a folder named picFolder to store
         // pics taken by the camera using this application.
-        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/";
+        final String dir = "/data/data/com.example.formation.prendrephoto/picFolder/";
         File newdir = new File(dir);
         newdir.mkdirs();
 
         Button capture = (Button) findViewById(R.id.btnCapture);
         capture.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
+                takephoto();
+
+                /*
 
                 // Here, the counter will be incremented each time, and the
                 // picture taken by camera will be stored as 1.jpg,2.jpg
@@ -61,17 +72,81 @@ public class MainActivity extends AppCompatActivity {
                 cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
 
                 startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+
+                */
+
             }
         });
+    }
+
+    public void takephoto() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
+                //result.setText("startActivityForResult");
+            }
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
-            Log.d("CameraDemo", "Pic saved");
+        if (requestCode == CAMERA_PIC_REQUEST && resultCode == RESULT_OK) {
+            //result.setText("1");
+
+            Bitmap thumbnail;
+            if (data != null) {
+                if (data.hasExtra("data")) {
+                    thumbnail = data.getParcelableExtra("data");
+                    ImageView image = (ImageView) findViewById(R.id.photoResultView);
+                    image.setImageBitmap(thumbnail);
+                    //result.setText("2");
+                }
+            } else {
+                // On sait ici que le fichier pointé par mFichier est accessible, on peut donc faire ce qu'on veut avec, par exemple en faire un Bitmap
+                Bitmap image = BitmapFactory.decodeFile(mCurrentPhotoPath);
+                thumbnail = ThumbnailUtils.extractThumbnail(image, 100, 100);
+
+                ImageView imageview = (ImageView) findViewById(R.id.photoResultView);
+                imageview.setImageBitmap(thumbnail);
+
+            }
         }
+    }
+
+
+    private File createImageFile() throws IOException {
+        //Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM);
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+
+        //result.setText(mCurrentPhotoPath);
+        return image;
     }
 
 }
